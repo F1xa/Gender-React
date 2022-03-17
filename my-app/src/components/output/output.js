81e7e5de26c9d031b205545498a82f5bda691487
F1xa <dubineckiy@bk.ru>
2022-03-18 02:03:58 +0300

    Updates

--- my-app/src/components/output/output.js
+++
@@ -1,9 +0,0 @@
-import { AddInput } from '../input/Input';
-import './output.css';
-
-export function Result(props) {
-  const { value } = props;
-  return (
-    <div className="container">{value}</div>
-  )
-}
